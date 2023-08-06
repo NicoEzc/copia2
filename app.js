0c8851f652cc5b9f32cce61b4dff62bf8fa9e3bc
NicoEzc <nicolasezcurra7gmail.com>
2023-08-06 20:27:27 -0300

    agregando archivos

---
+++ app.js
@@ -0,0 +1,31 @@
+const express= require("express")
+const path= require("path")
+const app=express();
+
+app.use(express.static("public"));
+
+app.listen(3000,()=> console.log("server on"));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+
+});
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+
+});
+
+app.get("/cart",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/cart.html"));
+
+});
+
+app.get("/detalleDeProducto",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/detalleDeProducto.html"));
+
+});
